fc913cadc09b323a75d60b18e6640274874cd464
bethatmarys <maria.f.delcastillo@gmail.com>
2017-05-12 11:35:01 -0700

    add back end functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,21 @@
-function Size(small, medium large){
+function Size(small, medium, large) {
   this.small = small;
   this.medium = medium;
   this.large = large;
   }
 
-function Toppings(pepperoni, ham, pinapple){
+function Topping(pepperoni, ham, pinapple){
   this.pepperoni = pepperoni;
   this.ham = ham;
   this.pinapple = sausage;
 }
 
 Size.prototype.pizzaSize = function() {
-  return this.small || this.medium || this.large;
+  return "You have chosen " + "#selectSize" + ".";
 }
 
 Topping.prototype.pizzaTopping = function() {
-  return this.pepperoni + ", " + this.ham + ", " + this.pinapple;
+  return "You have chosen " + "toppingChoice" + ".";
 }
 
 function resetFields() {
@@ -26,3 +26,40 @@ function resetFields() {
     $("input#ham").val("")
     $("input#sausage").val("")
 }
+$(document).ready(function() {
+  $("#pizzaChoice").submit(function(event) {
+  event.preventDefault();
+
+  var size = $("select#selectSize").val();
+  var topping = $("select#toppingChoice").val();
+
+  var newOrder = new Order(size, topping);
+
+  var price = 10;
+
+  if (size === "small") {
+    price += 1;
+  } else if (size === "medium") {
+    price += 5;
+  } else if (size === "large") {
+    price += 10;
+  }
+  if (topping === "pepperoni") {
+    price += 4;
+  } else if (topping === "ham") {
+    price += 2;
+  } else if (topping === "sausage") {
+    price += 6;
+  }
+
+
+$("#results").show();
+  // newTicket.movie.forEach(function(movie) {
+    $(".selectSize").text(newOrder.pizzaSize());
+    $(".price").text("$" + price + ".");
+    $(".toppingChoice").text(pizzaOrder.toppingChoice());
+    $(".price").text("$" + price + ".");
+
+    $(".toppingChoice").html("You have chosen a " + "<b>" + topping +  "</b>" + " pizza.");
+  });
+});
